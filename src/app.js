d1d210770122f110563a7291fc69e09f9fad9575
Dario Pisapia <worldariolibero.it>
2022-08-14 10:23:35 +0200

    added starting page and favicon

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
 import {cardGenerator, shuffler} from './cardGenerator.js'
 import {reset} from './domEvents.js'
-import {startTimer, stopInterval} from './timer.js'
+import {startTimer} from './timer.js'
 
-//generates the initial cards and starts timer
+//generates the initial cards
 cardGenerator(shuffler())
-startTimer()
 
 //add event listener to the restart buttons
+document.getElementById('startGame').addEventListener('click', () => {
+    startTimer()
+    document.getElementById('startingPage').style.display = 'none'
+})
+
 document.getElementById('restartMain').addEventListener('click', () => {
     reset()
 })
 document.getElementById('restartGame').addEventListener('click', () => {
     reset()
 })
-
 
 
